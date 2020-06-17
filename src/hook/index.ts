29f0bc1f3206d20608c5a6059417391904f8ca47
Sup T <>
2020-06-17 09:24:29 +0700

    feat(user): shown error when fetch failed

--- src/hook/index.ts
+++ src/hook/index.ts
@@ -1,11 +1,13 @@
 import { UseTweetFeedParams, APITweet, UseTweetFeed } from "types";
 import { useState, useEffect } from "react";
+import message from 'antd/lib/message';
 import api from '@api';
 import logger from '@common/utils/logger';
 
 const useTweetFeed = ({ q, offset, by }: UseTweetFeedParams): UseTweetFeed => {
   const [tweet, setTweet] = useState<APITweet>();
-  const [loading, setLoading] = useState<boolean>(false)
+  const [loading, setLoading] = useState<boolean>(false);
+  const displayError = 'Could not connect to server. Please check your internet connection and try again.';
 
   useEffect(() => {
     setLoading(true);
@@ -19,7 +21,8 @@ const useTweetFeed = ({ q, offset, by }: UseTweetFeedParams): UseTweetFeed => {
           setTweet(res)
         })
         .catch((err: Error) => {
-          logger.error('GET HashTag', err)
+          logger.error('GET HashTag', err);
+          message.error(displayError);
         })
         .finally(() => {
           setLoading(false);
@@ -35,6 +38,7 @@ const useTweetFeed = ({ q, offset, by }: UseTweetFeedParams): UseTweetFeed => {
         })
         .catch((err: Error) => {
           logger.error('GET HashTag', err)
+          message.error(displayError);
         })
         .finally(() => {
           setLoading(false);
